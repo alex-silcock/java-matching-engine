0622ad4ce4d3fc6069fc347374eac55bd735385c
alex-silcock <alexsilcock13@gmail.com>
2024-03-30 10:47:02 +0000

    add simple test cases

---
+++ src/test/OrderTest.java
@@ -0,0 +1,11 @@
+package test;
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+public class OrderTest {
+    @Test
+    public void test() {
+        System.out.println("Hello world");
+    }
+
+}
